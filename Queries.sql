d4bf0549d1b3a3d4dc09b062247730db4373526d
Nitish Kumar <62009174+nitishkumar2701@users.noreply.github.com>
2026-02-17 16:04:46 +0000

    Create Queries.sql

---
+++ Queries.sql
@@ -0,0 +1,6 @@
+SELECT TOP (5) * FROM [dbo].[Fact_Trade]
+SELECT top (5) * FROM [dbo].[Dim_Trade_Group]
+SELECT top (5) * FROM [dbo].[Dim_Country]
+SELECT top (5) * FROM [dbo].[Dim_Trade_Type]
+SELECT top (5) * FROM [dbo].[Dim_Country]
+
